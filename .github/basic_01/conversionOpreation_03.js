3f7f1b9916e4bb825f0a316de56cf41d26ac5980
Prince Kashap <156696690+princekashap@users.noreply.github.com>
2025-01-08 16:09:18 +0000

    Conversion of datatypes

---
+++ .github/basic_01/conversionOpreation_03.js
@@ -0,0 +1,24 @@
+let number =    null
+console.log(typeof number);
+console.log(typeof(number))
+
+
+let valueNumber = Number(number)
+console.log(typeof (valueNumber));
+console.log(valueNumber)
+
+
+//"25" =>  25
+//"25abc" => NaN
+// "true" => 1 / false => 0
+
+let isLoggedin = 0
+
+let boolean = Boolean(isLoggedin)
+console.log(typeof boolean);
+
+let sumnumber = 33
+let StringName = String(sumnumber)
+console.log(StringName);
+console.log(typeof StringName);
+
