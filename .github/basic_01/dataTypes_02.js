b01bd52d39a1b4b9d5c9a51ac657546a81892291
Prince Kashap <156696690+princekashap@users.noreply.github.com>
2025-01-08 16:07:45 +0000

    Conversion of datatypes

---
+++ .github/basic_01/dataTypes_02.js
@@ -0,0 +1,22 @@
+let age =25
+let Name = "Rahul"
+let Isloggedin = true
+let state
+
+
+
+// Number => 
+// String => ""
+// Boolean => true/false
+// null => stand alone value
+// undefine => In current time there is no value is assing
+// symbol => Find uniqness
+
+// object 
+
+console.log(typeof null);   // object
+
+console.log(typeof undefined);  // undefine
+
+
+console.log(typeof "prince");
