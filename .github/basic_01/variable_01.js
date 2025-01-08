8cd8d964e85b8c5cf3a01e2f6e9c7773a296c2db
Prince Kashap <156696690+princekashap@users.noreply.github.com>
2025-01-08 16:09:41 +0000

    variables

---
+++ .github/basic_01/variable_01.js
@@ -0,0 +1,13 @@
+const accountholderId =254254;
+
+// We can not over write it.
+
+let accountholderName = "Prince kashyap";
+
+let accountholderCity = "Hathras";
+
+let state
+
+console.table([accountholderId,accountholderName,accountholderCity,state]);
+
+// In present day we can not use var variable because of scope of block of code or scope functionality
