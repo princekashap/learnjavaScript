81db9cf4e7759bb57552fbd0c244c4f594dfd70b
Prince Kashyap <156696690+princekashap@users.noreply.github.com>
2025-01-11 16:24:50 +0000

    Math & Num

---
+++ .github/basic_01/numMath_04.js
@@ -0,0 +1,44 @@
+const  score = 5455
+//console.log(score);
+
+const balance = new Number(4512)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+
+//console.log(balance.toFixed(1));
+
+const othernumber = 75.58
+//console.log(othernumber.toPrecision(3));
+
+const anyvalue = 1000000000
+//console.log(anyvalue.toLocaleString());
+
+
+// +*+*+*+*+*+**+*+*+*++**+*+*++*+*+*+**+*+*+*++*+*+*+**+*+* Math *+*+*+*+*+*+*+*+*+*+*+*+**++*+*+*+*+**+*+*+*+*+*+*+*+*++*
+
+console.log(Math);
+
+console.log(Math.abs(-95));
+console.log(Math.floor(4.7));
+console.log(Math.ceil(4.1));
+console.log(Math.min(4,5,5,6,6,4,1));
+console.log(Math.max(4,75,85,65,6,5,20,30,100,225))
+
+console.log(Math.round(69.7));
+
+console.log((Math.random()*10)+1);
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()* max-min)+min);
+
+
+
+
+
+
+
