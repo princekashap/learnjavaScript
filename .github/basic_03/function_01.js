a3b6e3c7b5d4d61deacad883d4a56e685ca9639b
Prince Kashyap <156696690+princekashap@users.noreply.github.com>
2025-01-16 16:12:36 +0000

    Done

--- .github/basic_03/function_01.js
+++ .github/basic_03/function_01.js
@@ -9,12 +9,12 @@
 // }
 // printname()
 
-function addtwonumbers(num1,num2){
-  //  let number = num1+num2
-    return num1+num2
-}
- const number =addtwonumbers(5,5)
- console.log("result is:",number)
+// function addtwonumbers(num1,num2){
+//   //  let number = num1+num2
+//     return num1+num2
+// }
+//  const number =addtwonumbers(5,5)
+//  console.log("result is:",number)
 
 // function loginmessage(username){
 //     return `${username} just logged in`
@@ -24,11 +24,32 @@ function addtwonumbers(num1,num2){
 //console.log(loginmessage("Prince"))
 
 
-function message(username){
-if(username===undefined){
-    console.log( "Please enter the username")
-    return
+// function message(username){
+// if(username===undefined){
+//     console.log( "Please enter the username")
+//     return
+// }
+// return `${username} just logged in`
+// }
+// console.log(message("Prince"));
+
+function addcardnumber(val1,val2,...number1){
+  return number1
+
 }
-return `${username} just logged in`
+//console.log(addcardnumber(5,6,90,87,45));
+
+const user={
+  username:"Aditya",
+  city:"Hathras"
+
+
 }
-console.log(message("Prince"));
+console.log(user);
+
+function handleObject(anyobject){
+  console.log(`Username is ${user.username} and he lived in ${user.city}`);
+  
+}
+handleObject(user)
+
